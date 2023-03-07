8df65c9a6f5221fc22d0a747fd8a9ec5d95cb666
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-03-07 22:46:14 +0300

    The first version of this project!

---
+++ my_math.js
@@ -0,0 +1,12 @@
+const add = (a, b) => {
+    return a + b
+}
+const minus = (a, b) => {
+    return a - b
+}
+module.exports = {
+    add: (a, b) => {
+        return a + b},
+    minus: (a, b) => {
+        return a - b}
+}
